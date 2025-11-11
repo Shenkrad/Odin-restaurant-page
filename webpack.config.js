294e0c1d4adb2654080d0ec027ce6b4e4389a80f
shenkrad <58443488+Shenkrad@users.noreply.github.com>
2025-11-11 20:25:41 +0100

    feat: main site styles and scripts

--- webpack.config.js
+++ webpack.config.js
@@ -25,12 +25,12 @@ module.exports = {
             use: ["style-loader", "css-loader"],
         },
         {
-        test: /\.html$/i,
-        loader: "html-loader",
+            test: /\.html$/i,
+            loader: "html-loader",
         },
         {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+            test: /\.(png|svg|jpg|jpeg|gif|avif|ttf|woff2|woff)$/i,
+            type: "asset/resource",
         },
     ],
   },
